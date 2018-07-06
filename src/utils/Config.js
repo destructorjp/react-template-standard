bdb6d0b9c2cc2e4306259dda39be458248640441
ito-p <not.1010.number@gmail.com>
2018-07-07 04:33:24 +0900

    use .env.development.local

--- src/utils/Config.js
+++ src/utils/Config.js
@@ -2,10 +2,8 @@ require('dotenv').config();
 
 const env = process.env;
 
-const { NODE_ENV } = env;
-
 const config = Object.keys(env).reduce((acc, key) => {
-  const reg = new RegExp(`^REACT_APP_${NODE_ENV.toUpperCase()}_(.+)`);
+  const reg = new RegExp(`^REACT_APP_(.+)`);
 
   if (key.match(reg)) {
     return {
