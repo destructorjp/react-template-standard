6b29b16bb2b9a57d39d479876b2c9e50a0e9322a
ito-p <not.1010.number@gmail.com>
2018-07-07 05:07:17 +0900

    Add firebase

---
+++ src/utils/Firebase.js
@@ -0,0 +1,14 @@
+import firebase from 'firebase';
+
+import Config from './Config';
+
+const config = {
+  apiKey: Config.FIREBASE_API_KEY,
+  authDomain: Config.FIREBASE_AUTH_DOMAIN,
+  databaseURL: Config.FIREBASE_DATABASE_URL,
+  projectId: Config.FIREBASE_PROJECT_ID,
+  storageBucket: Config.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: Config.FIREBASE_MESSAGING_SENDOR_ID
+};
+
+firebase.initializeApp(config);
